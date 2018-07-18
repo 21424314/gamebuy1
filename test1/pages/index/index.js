908dc92cdc1c2688f5016ed116d6400df33df738
21424314 <41364015+21424314@users.noreply.github.com>
2018-07-18 20:35:29 +0800

    Add files via upload

---
+++ test1/pages/index/index.js
@@ -0,0 +1,19 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    autoplay:true,
+    interval:5000,
+    duration:1000,
+    circular:true,
+    imgUrls:[
+      "/images/haibao/00.jpg",
+      "/images/haibao/01.jpg", 
+      "/images/haibao/10.jpg"
+    ]
+  }
+})
